b1c4822e3bb0c6139c74ff878e44c03f90b8c383
ovidiudod <ovidiudod@gmail.com>
2015-07-23 23:02:35 +0200

    Lista preguntas

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+// Página de entrada (home page)
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+module.exports = router;
